71453780ffa0f229320861aae9ddae689e642409
Llamacles <jsabuyuan@gmail.com>
2022-12-19 18:55:41 -0800

    Movement 1.0
    Movement Added

---
+++ BulletSmell/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    [SerializeField] private float moveSpeed = 5f;
+    public PlayerInputActions playerControls;
+
+    Vector2 moveDirection = Vector2.zero;
+    private InputAction move;
+
+    private void Awake()
+    {
+        playerControls = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        move = playerControls.Player.Move;
+        move.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Enable();
+        move.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = move.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
