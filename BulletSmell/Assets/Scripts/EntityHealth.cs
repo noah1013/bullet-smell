2391208663ff3471f7b194b5b49af5ff384673c7
Toimis <74384377+Toimis@users.noreply.github.com>
2022-12-25 23:36:50 -0800

    v0.0.10.8
    scene changes and dying goes to main screen (needs Bullet Time debugging)

--- BulletSmell/Assets/Scripts/EntityHealth.cs
+++ BulletSmell/Assets/Scripts/EntityHealth.cs
@@ -1,31 +1,42 @@
+using JetBrains.Annotations;
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EntityHealth : MonoBehaviour
 {
     /////////public static event Action<EntityHealth> OnEntityKilled; 
-    [SerializeField] private int health, maxHealth = 2;
+    [SerializeField] private int currentHealth, maxHealth = 2;
 
     // Start is called before the first frame update
     void Start()
     {
-        health = maxHealth;
+        currentHealth = maxHealth;
     }
 
     public void TakeDamage(int damageAmount)
     {
-        health -= damageAmount;
+        currentHealth -= damageAmount;
 
-        if (health <= 0)
+        if (currentHealth <= 0)
         {
             if (TryGetComponent<PlayerMovement>(out PlayerMovement playerMovementComponent)) // testing if the entity is a player
             {
                 // transition to a death scene or something
-
+                SceneManager.LoadScene("MainMenu");
             }
 
             Destroy(gameObject);
             /////////OnEntityKilled?.Invoke(this);
+        }
+    }
+    public void Heal(int healAmount)
+    {
+        currentHealth += healAmount;
+
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
         }
 
     }
